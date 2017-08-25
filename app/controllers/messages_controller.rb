930053350eb48d95554023c208691f068d5887f1
Jhonattan Rodriguez <jjrodriguezn@unal.edu.co>
2017-08-25 17:16:39 -0500

    Messages implementado en chat rooms

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def _message
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/_message'
+
   get 'chat_rooms/index'
 
   get 'chat_rooms/new'
@@ -7,6 +13,8 @@ Rails.application.routes.draw do
 
   get 'chat_rooms/show'
 
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
