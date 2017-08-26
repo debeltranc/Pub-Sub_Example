0ff61fb2ffba640ce06b5c24b87e750fb9e2e889
Helmer Avendaño <haavendanov@unal.edu.co>
2017-08-26 01:51:41 -0500

    attempt 1 fix heroic app

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
-  get 'messages/show'
-
-  get 'messages/_message'
-
-  get 'chat_rooms/index'
-
-  get 'chat_rooms/new'
-
-  get 'chat_rooms/create'
-
-  get 'chat_rooms/show'
-
-  resources :chat_rooms, only: [:new, :create, :show, :index]
-  root 'chat_rooms#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+
   mount ActionCable.server => '/cable'
+
+  root 'chat_rooms#index'
 end
